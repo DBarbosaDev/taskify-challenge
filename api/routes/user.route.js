6c71ee17ac5ad6c31f8808945ade9e98e0c262e4
unknown <dev@buildtoo.com>
2021-03-27 16:38:42 +0000

    feature: API: User authentication

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -7,5 +7,6 @@ const { authValidator } = require('../validators');
 const { authMiddleware } = require('../middlewares');
 
 router.post('/regist', authValidator.validateRegistParams(), authMiddleware.checkEmailExistance, authController.regist);
+router.post('/login', authValidator.validateLoginParams(), authMiddleware.checkLoginParams, authController.login);
 
 module.exports = router;
--- api/services/auth.service.js
+++ api/services/auth.service.js
@@ -1,4 +1,7 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const configs = require('../../configs');
 
 const UserModel = require('../models/user.model');
 
@@ -20,6 +23,26 @@ const regist = async (details) => {
     return user;
 };
 
+const login = async (requesterIp = null, details = {}) => {
+    const { _id, name, surname } = details;
+
+    const tokenCreationPromise = new Promise((resolve, reject) => {
+        jwt.sign({ _id, ipAddress: requesterIp }, configs.JWT_PRIVATE_KEY, { expiresIn: '24h' }, (err, token) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(token);
+        });
+    });
+
+    return {
+        name,
+        surname,
+        token: await tokenCreationPromise
+    };
+};
+
 module.exports = {
-    regist
+    regist,
+    login
 };
